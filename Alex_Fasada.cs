e72a69136db1dfd26ee4dbb5260939ef4ea38735
Jakub Łabendzki <113890308+Jakub-Labendzki@users.noreply.github.com>
2024-04-08 13:32:34 +0200

    Create Alex_Fasada.cs

---
+++ Alex_Fasada.cs
@@ -0,0 +1,30 @@
+    class Aktywność
+    {
+        private string typ;
+        public string czas;
+    }
+
+    class User
+    {
+            
+    }
+
+    class Pracownik : User
+    {
+        private string stanowisko;
+        public string wypłata;
+    }
+
+    class Adres
+    {
+        public string ulica;
+        public string kod;
+        public string miasto;
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
